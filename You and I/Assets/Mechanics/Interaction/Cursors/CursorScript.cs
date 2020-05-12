510753f042a32d8eebd89ecd15a2bd3585f0718d
Xavier Hancock <Arbiter04032000@gmail.com>
2020-05-12 15:07:43 +1000

    Controllers + movement

---
+++ You and I/Assets/Mechanics/Interaction/Cursors/CursorScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental;
+
+public class CursorScript : MonoBehaviour
+{
+    public int controllerNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(Input.GetAxis("Horizontal" + controllerNo), 0, 0);
+        this.gameObject.transform.Translate(0, -(Input.GetAxis("Vertical" + controllerNo)), 0);
+
+
+    }
+}
